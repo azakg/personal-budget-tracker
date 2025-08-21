9ad67ea3b22bf6395c2b53963ab9ef449d5923a8
azakg <azakgzuu@gmail.com>
2025-08-21 09:48:18 -0400

    main.tf

---
+++ infra/terraform/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = { source = "hashicorp/aws", version = ">= 5.0" }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name    = "${var.project}-vpc"
+  cidr    = "10.0.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.0"
+  cluster_name    = "${var.project}-eks"
+  cluster_version = var.eks_version
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
+      instance_types = ["t3.small"]
+    }
+  }
+}
+
+resource "aws_ecr_repository" "app" {
+  name                 = "${var.project}-api"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+}
+
+output "cluster_name"   { value = module.eks.cluster_name }
+output "ecr_url"        { value = aws_ecr_repository.app.repository_url }
+output "kubeconfig_cmd" {
+  value = "aws eks update-kubeconfig --region ${var.region} --name ${module.eks.cluster_name}"
+}
+
