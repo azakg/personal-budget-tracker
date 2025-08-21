43c7d83edb13477f9a56abdd51bcf52b6d255b31
azakg <azakgzuu@gmail.com>
2025-08-21 10:18:44 -0400

    delete s3

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "project"     { type = string  default = "budget" }
+variable "region"      { type = string  default = "us-east-1" }
+variable "eks_version" { type = string  default = "1.29" }
+
