a74c11940a2505334aa74796ac679945dee635fa
azakg <azakgzuu@gmail.com>
2025-09-03 05:51:04 -0400

    terraform

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,4 +1,16 @@
-variable "project"     { type = string  default = "budget" }
-variable "region"      { type = string  default = "us-east-1" }
-variable "eks_version" { type = string  default = "1.29" }
+variable "project" {
+  type    = string
+  default = "budget"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "eks_version" {
+  type    = string
+  default = "1.29"
+}
+
 
